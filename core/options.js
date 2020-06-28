69558f35d36fa7cb5972f25bf3cfdc2949b6f223
Kody Wiremane <kody@wiremane.net>
2020-06-29 00:44:35 +0300

    Add page for configuring applied modifications, add /settings/apps widget

---
+++ core/options.js
@@ -0,0 +1,174 @@
+(() => {
+    'use strict';
+
+    const config = new Config();
+    const log = new Logger('ESK:CFG')
+    config.get({
+        'ui-augments.add-thumbs-with-ordinal-indices': true,
+        'ui-augments.add-um-profile-links': true,
+        'ui-augments.enable-better-watch-indi': true,
+        'ui-patches.enable-dva-img-pe': true,
+        'ui-patches.fix-blocking-tooltips': true,
+        'ui-patches.fix-comment-avatar-ghost-link': true,
+        'ui-patches.fix-um-fallout': true
+    }, spawnUi)
+
+    function spawnUi(configuration) {
+
+        const container = document.getElementById('container');
+        container.addEventListener("change", onControlChange);
+
+        const gNote = createOptionGroup('Note');
+        gNote.appendChild(createOptionText(
+            'New configuration is read on page load. Refresh your DA tabs to apply new settings.')
+        );
+        const gUiAugments = createOptionGroup('UI Augments');
+        const gUiPatches = createOptionGroup('UI Patches');
+
+        [
+            gNote,
+            gUiAugments,
+            gUiPatches
+        ]
+        .forEach(group => container.appendChild(group));
+
+        [
+            createConfigCheckbox(
+                'ui-augments.add-um-profile-links',
+                'Add profile section links to the user menu',
+                configuration['ui-augments.add-um-profile-links']
+            ),
+            createConfigCheckbox(
+                'ui-augments.enable-better-watch-indi',
+                'Make watch update mark more prominent',
+                configuration['ui-augments.enable-better-watch-indi']
+            ),
+            createConfigCheckbox(
+                'ui-augments.add-thumbs-with-ordinal-indices',
+                'Add ordinal numbers to collection/gallery thumbnails',
+                configuration['ui-augments.add-thumbs-with-ordinal-indices']
+            )
+        ]
+        .forEach(item => gUiAugments.appendChild(item));
+
+        [
+            createConfigCheckbox(
+                'ui-patches.fix-um-fallout',
+                'Prevent invisible buttons from triggering the user menu fallout',
+                configuration['ui-patches.fix-um-fallout']
+            ),
+            createConfigCheckbox(
+                'ui-patches.enable-dva-img-pe',
+                'Enable mouse interaction with image deviations',
+                configuration['ui-patches.enable-dva-img-pe']
+            ),
+            createConfigCheckbox(
+                'ui-patches.fix-blocking-tooltips',
+                'Prevent tooltips from blocking underlying things',
+                configuration['ui-patches.fix-blocking-tooltips']
+            ),
+            createConfigCheckbox(
+                'ui-patches.fix-comment-avatar-ghost-link',
+                'Remove the invisible link under comments\' userpic',
+                configuration['ui-patches.fix-comment-avatar-ghost-link']
+            )
+        ]
+        .forEach(item => gUiPatches.appendChild(item));
+    }
+
+
+
+    function createOptionGroup(label) {
+        const group = document.createElement('section');
+        group.className = 'option-group';
+        group.setAttribute('data-label', label);
+        return group;
+    }
+
+    function createOptionSeparator() {
+        const separator = document.createElement('hr');
+        separator.className = 'option-separator';
+        return separator;
+    }
+
+    function createOptionText(text) {
+        const wrapper = document.createElement('div');
+        wrapper.className = 'option-text';
+        wrapper.appendChild(document.createTextNode(text));
+        return wrapper;
+    }
+
+    function createConfigCheckbox(name, label, checked = false) {
+        const control = document.createElement('label');
+        control.className = 'control-wrapper';
+        control.insertAdjacentHTML(
+            'afterbegin',
+            `<input type="checkbox" class='control-control' data-config-control="${name}"
+            ${checked ? 'checked' : ''}><span class="control-label">${label}</span>`
+        );
+        return control;
+    }
+
+    function createConfigRadio(name, label, value = null, selected = false) {
+        const control = document.createElement('label');
+        control.className = 'control-wrapper';
+        control.insertAdjacentHTML(
+            'afterbegin',
+            `<input type="radio" name="${name}" class="control-control" data-config-control="${name}"
+            ${value !== null ? `value="${value}"` : ''} ${selected ? 'checked' : ''}
+            ><span class="control-label">${label}</span>`
+        );
+        return control;
+    }
+
+    function createConfigRadioGroup(caption, name, radios, selected = null) {
+        const wrapper = document.createElement('div');
+        wrapper.className = 'radio-group';
+        wrapper.appendChild(createOptionText(caption));
+        radios.forEach(
+            radio => wrapper.appendChild(
+                createConfigRadio(name, radio.label, radio.value, radio.value && radio.value === selected)
+            )
+        );
+        return wrapper;
+    }
+
+
+
+    function onControlChange(event) {
+        const target = event.target;
+        const controlName = getControlName(target)
+        const controlValue = getControlValue(target);
+        const request = {};
+        request[controlName] = controlValue;
+        config.set(request, () => config.lastError
+            ? log.error(`Failed to store ${controlName} setting`)
+            : log.info(`Successfully stored ${controlName} setting`)
+        );
+    }
+
+    function getControlName(control) {
+        return control.hasAttribute('data-config-control') ?
+            control.getAttribute('data-config-control') : undefined;
+    }
+
+    function getControlValue(control) {
+        switch (control.tagName.toLowerCase()) {
+            case 'input':
+                switch (control.type) {
+                    case 'checkbox':
+                        return control.checked;
+                        break;
+                    case 'radio':
+                        return control.value;
+                        break;
+                    default:
+                        return undefined;
+                }
+                break;
+            default:
+                return undefined;
+        }
+    }
+
+})()
--- js/lib/utils.js
+++ js/lib/utils.js
@@ -1,8 +1,60 @@
 'use strict';
 
+/* TYPE CHECKS */
+
 function isUndefined(target) {
     return typeof target === 'undefined';
 }
+
+function isString(target) {
+    return typeof target === 'string';
+}
+
+function isFunction(target) {
+    return typeof target === 'function';
+}
+
+function isNull(target) {
+    return target === null;
+}
+
+function isArray(target) {
+    return Array.isArray(target);
+}
+
+function isObject (target) {
+    return typeof target === 'object'
+    && !isNull(target)
+    && !isArray(target);
+}
+
+function phpType(target) {
+    const nativeType = typeof target;
+    switch (nativeType) {
+        case 'undefined':
+        case 'boolean':
+        case 'number':
+        case 'bigint':
+        case 'string':
+        case 'symbol':
+        case 'function':
+            return nativeType;
+            break;
+        case 'object':
+            if (target === null) {
+                return 'null';
+            }
+            if (Array.isArray(target)) {
+                return 'array';
+            }
+            return 'object';
+            break;
+        default:
+            throw 'unknown type';
+    }
+}
+
+/* STRING OPERATIONS */
 
 function splitStringOnce(string, separator) {
     const [left, ...rights] = string.split(separator);
@@ -13,6 +65,68 @@ function trimTupleLeft(tuple) {
     tuple[0] = tuple[0].trim();
     return tuple;
 }
+
+/* CONTAINER OPERATIONS */
+
+function prefixArrayValues(target, valuePrefix) {
+    if (!isArray(target)) {
+        throw 'target must be array';
+    }
+    if (!isString(valuePrefix)) {
+        throw 'valuePrefix must be string'
+    }
+    return target.map((value) => `${valuePrefix}${value}`);
+}
+
+function prefixObjectKeys(target, keyPrefix) {
+    if (!isObject(target)) {
+        throw 'target must be object';
+    }
+    if (!isString(keyPrefix)) {
+        throw 'keyPrefix must be string'
+    }
+    return Object.entries(target).reduce((mapped, keyValuePair) => {
+        let [key, value] = keyValuePair;
+        mapped[`${keyPrefix}${key}`] = value;
+        return mapped;
+    }, {});
+}
+
+function deprefixObjectKeys(target, keyPrefix) {
+    if (!isObject(target)) {
+        throw 'target must be object';
+    }
+    if (!isString(keyPrefix)) {
+        throw 'keyPrefix must be string'
+    }
+    return Object.entries(target).reduce((mapped, keyValuePair) => {
+        let [key, value] = keyValuePair;
+        mapped[key.startsWith(keyPrefix) ? key.slice(keyPrefix.length) : key] = value;
+        return mapped;
+    }, {});
+}
+
+function filterObjectKeyPrefix(target, keyPrefix) {
+    if (!isObject(target)) {
+        throw 'target must be object';
+    }
+    if (!isString(keyPrefix)) {
+        throw 'keyPrefix must be string'
+    }
+    return Object.entries(target).reduce((mapped, keyValuePair) => {
+        let [key, value] = keyValuePair;
+        if (key.startsWith(keyPrefix)) {
+            mapped[key] = value;
+        }
+        return mapped;
+    }, {});
+}
+
+function filterAndDeprefixObjectKeys(target, keyPrefix) {
+    return deprefixObjectKeys(filterObjectKeyPrefix(target, keyPrefix), keyPrefix);
+}
+
+/* DA INTERFACE */
 
 function getUsernameFromCookies() {
     const cookies = document.cookie.split(';').reduce((cookies, cookieString) => {
