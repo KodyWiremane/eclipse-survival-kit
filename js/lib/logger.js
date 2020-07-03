21495e1b86d30ce9724dd187001d5f99d3945d54
Kody Wiremane <kody@wiremane.net>
2020-07-03 13:28:22 +0300

    Patch class members' bracing style

--- js/lib/logger.js
+++ js/lib/logger.js
@@ -1,25 +1,31 @@
 class Logger {
-    constructor (scopeName) {
+    constructor(scopeName)
+    {
         this.scopeName = `[${scopeName}]`;
     }
 
-    log (levelName, message, ...extras) {
+    log(levelName, message, ...extras)
+    {
         console.log(`${this.scopeName}:${levelName}: ${message}`, ...extras);
     }
 
-    debug (message, ...extras) {
+    debug(message, ...extras)
+    {
         this.log('DEBUG', message, ...extras);
     }
 
-    info (message, ...extras) {
+    info(message, ...extras)
+    {
         this.log('INFO', message, ...extras);
     }
 
-    warning (message, ...extras) {
+    warning(message, ...extras)
+    {
         this.log('WARNING', message, ...extras);
     }
 
-    error (message, ...extras) {
+    error(message, ...extras)
+    {
         this.log('ERROR', message, ...extras);
     }
 }
